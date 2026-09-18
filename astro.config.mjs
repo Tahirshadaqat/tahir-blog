ecaa8e0f72671da5a480d5a060ea0a7ee9b4c025
Tahir <sadaqat.tahir@gmail.com>
2026-09-19 01:27:19 +0700

    Fix Astro and Cloudflare version compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
+  output: "server",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
